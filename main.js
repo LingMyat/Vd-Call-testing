494afc6415be8648b567db1d5bb02d7d170a5664
LingMyat223 <lingmyataung@outlook.com>
2023-01-03 09:14:38 +0630

    Feature Perfect

--- main.js
+++ main.js
@@ -1,50 +1,183 @@
-let APP_ID = 'e2bacbaed91b435886184748f9b22434';
+let APP_ID = "ff5a10dda27941659db490997bdb3be7"
+
 
 let token = null;
+let uid = String(Math.floor(Math.random() * 10000))
+
+let client;
+let channel;
+
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room')
+
+if(!roomId){
+    window.location = 'lobby.html'
+}
 
 let localStream;
 let remoteStream;
 let peerConnection;
+
 const servers = {
     iceServers:[
         {
-            urls:['stun:stun1.l.google.com:19302','stun:stun2.l.google.com:19302']
+            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
         }
     ]
 }
 
-let init = async()=>{
-    localStream = await navigator.mediaDevices.getUserMedia({video:true,audio:false});// to get browser camera and mic
-    document.getElementById('user-1').srcObject=localStream;
-    createOffer();
+
+let constraints = {
+    video:{
+        width:{min:640, ideal:1920, max:1920},
+        height:{min:480, ideal:1080, max:1080},
+    },
+    audio:true
 }
 
-let createOffer = async()=>{
-    peerConnection = new RTCPeerConnection(servers);
+let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
 
-    remoteStream = new MediaStream();
-    document.getElementById('user-2').srcObject=remoteStream;
+    channel = client.createChannel(roomId)
+    await channel.join()
 
-    localStream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track,localStream);
-    });
+    channel.on('MemberJoined', handleUserJoined)
+    channel.on('MemberLeft', handleUserLeft)
 
-    peerConnection.ontrack = (event)=>{
-        event.stream[0].getTracks().forEach((track)=>{
-            remoteStream.addTrack()
-        })
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
+    localStream = await navigator.mediaDevices.getUserMedia(constraints)
+    document.getElementById('user-1').srcObject = localStream
+}
+ 
+
+let handleUserLeft = (MemberId) => {
+    document.getElementById('user-2').style.display = 'none'
+    document.getElementById('user-1').classList.remove('smallFrame')
+}
+
+let handleMessageFromPeer = async (message, MemberId) => {
+
+    message = JSON.parse(message.text)
+
+    if(message.type === 'offer'){
+        createAnswer(MemberId, message.offer)
     }
 
-    peerConnection.onicecandidate = async (event)=>{
-        if(event.candidate){
-            console.log('New Ice Candidate:',event.candidate);
+    if(message.type === 'answer'){
+        addAnswer(message.answer)
+    }
+
+    if(message.type === 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate)
         }
     }
 
-    let offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
 
-    console.log('Offer:',offer);
 }
 
-init();
+let handleUserJoined = async (MemberId) => {
+    console.log('A new user joined the channel:', MemberId)
+    createOffer(MemberId)
+}
+
+
+let createPeerConnection = async (MemberId) => {
+    peerConnection = new RTCPeerConnection(servers)
+
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+    document.getElementById('user-2').style.display = 'block'
+
+    document.getElementById('user-1').classList.add('smallFrame')
+
+
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+        document.getElementById('user-1').srcObject = localStream
+    }
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
+    })
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track)
+        })
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
+        }
+    }
+}
+
+let createOffer = async (MemberId) => {
+    await createPeerConnection(MemberId)
+
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
+}
+
+
+let createAnswer = async (MemberId, offer) => {
+    await createPeerConnection(MemberId)
+
+    await peerConnection.setRemoteDescription(offer)
+
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+}
+
+
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
+    }
+}
+
+
+let leaveChannel = async () => {
+    await channel.leave()
+    await client.logout()
+}
+
+let toggleCamera = async () => {
+    let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
+
+    if(videoTrack.enabled){
+        videoTrack.enabled = false
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+    }else{
+        videoTrack.enabled = true
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+    }
+}
+
+let toggleMic = async () => {
+    let audioTrack = localStream.getTracks().find(track => track.kind === 'audio')
+
+    if(audioTrack.enabled){
+        audioTrack.enabled = false
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+    }else{
+        audioTrack.enabled = true
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+    }
+}
+  
+window.addEventListener('beforeunload', leaveChannel)
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
+
+init()
